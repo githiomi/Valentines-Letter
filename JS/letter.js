005d4459ee012a68c7ac2ea7e02d36196b7f3bbe
githiomi <danielgithiomi@gmail.com>
2024-02-06 21:21:27 +0400

    Feat: Added GIF for reading letter

--- JS/letter.js
+++ JS/letter.js
@@ -1,11 +1,17 @@
 // LETTER
-const envelope = document.querySelector('.envelope');
+const gun_gif = document.querySelector('.gun-gif');
 const letter = document.querySelector('.letter');
+const envelope = document.querySelector('.envelope');
 
 envelope.addEventListener('mouseover', () => {
     letter.classList.add('over-letter')
+    gun_gif.classList.remove('show')
 });
 
 envelope.addEventListener('mouseout', () => {
     letter.classList.remove('over-letter')
+
+    setTimeout(() => {
+        gun_gif.classList.add('show');
+    }, 1500);
 });
