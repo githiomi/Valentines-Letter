06457291a8618a42465747a4e8c7f204f425b36f
githiomi <danielgithiomi@gmail.com>
2024-02-06 15:44:33 +0400

    Feat: Completed basic layout of the page

---
+++ JS/index.js
@@ -0,0 +1,35 @@
+// Typewriter Effect
+
+// Scroll Reveal
+const scroll_reveal = ScrollReveal ({
+    reset: true,
+    distance: '100px',
+    duration: 2000,
+    delay: 10
+});
+
+scroll_reveal.reveal('.name', {origin: 'top'});
+scroll_reveal.reveal('.home-content', {origin: 'left'});
+scroll_reveal.reveal('.socials, .cv', {origin: 'right'});
+
+// Buttons
+const noButton = document.getElementById("no")
+const yesButton = document.getElementById("yes")
+
+// noButton.addEventListener('mouseover', (event) =>{
+//     const x = noButton.offsetLeft;
+//     const y = noButton.offsetTop
+
+//     // Generate random positions
+//     const newX = Math.floor(Math.random() * x);
+//     const newY = Math.floor(Math.random() * y);
+
+//     // Limit newY to the height of the HTML document
+//     const maxAllowedY = document.documentElement.clientHeight - noButton.clientHeight;
+//     const limitedY = Math.min(newY, maxAllowedY);
+
+//     // Set the new position
+//     noButton.style.left = newX + 'px';
+//     noButton.style.top = limitedY + 'px';
+
+// })
