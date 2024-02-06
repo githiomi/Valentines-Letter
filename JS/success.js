9c32080f1225bdcf63f070a61af1a4f9604f9cf9
githiomi <danielgithiomi@gmail.com>
2024-02-06 20:23:12 +0400

    Feat: Completed the success page and routing to letter page

---
+++ JS/success.js
@@ -0,0 +1,6 @@
+// Can't wait button
+const wait = document.getElementById("same").addEventListener("click", (e) => {
+    alert("Same! See you then bitch ❤️!");
+
+    window.location.href = './letter.html';
+});
